e6faadf922d94d55afb60f18c05db2a7d9296fa3
Patrik Pagac <patrik.pagac@gmail.com>
2025-09-26 21:35:23 +0200

    Add files via upload

--- 04.sql
+++ 04.sql
@@ -2,30 +2,32 @@
 
 with FoodGrowth  as(
 	select 
-			payroll_year,
-		    round(
-          ((avg(price) - lag(avg(price)) over (order by payroll_year))
-         / lag(avg(price)) over (order by payroll_year))::numeric* 100, 2  )as food_YoyGrowth
+		payroll_year,
+	    round(
+		      ((avg(price) - lag(avg(price)) over (order by payroll_year))
+		     / lag(avg(price)) over (order by payroll_year))::numeric* 100
+     , 2  )as food_YoyGrowth
 	from t_patrik_pagac_project_SQL_primary_final
 	group by  payroll_year
 	),
 	WageGrowth  as(
     select
-    payroll_year,
-    round(
-            ((avg(average_wages) - lag(avg(average_wages)) over (order by payroll_year))
-             / lag(avg(average_wages)) over (order by payroll_year)) * 100, 2  ) as wage_YoyGrowth
-    from t_patrik_pagac_project_SQL_primary_final
+    	payroll_year,
+    	round(
+		        ((avg(average_wages) - lag(avg(average_wages)) over (order by payroll_year))
+		        / lag(avg(average_wages)) over (order by payroll_year)) * 100
+        , 2  ) as wage_YoyGrowth
+	from t_patrik_pagac_project_SQL_primary_final
     where calculation_code = 200
     group by payroll_year
     )
-    select
-    	w.	payroll_year,
-		f.food_YoyGrowth,
-		w.	wage_YoyGrowth,
-		 f.food_YoyGrowth -w.wage_YoyGrowth as  difference
-    from FoodGrowth f
-    join WageGrowth w
-    on w.payroll_year=f.payroll_year
-    --where f.food_YoyGrowth -w.wage_YoyGrowth >5
-    order by difference desc 
+select
+	w.payroll_year,
+	f.food_YoyGrowth,
+	w.wage_YoyGrowth,
+	 f.food_YoyGrowth -w.wage_YoyGrowth as  difference
+from FoodGrowth f
+join WageGrowth w
+	on w.payroll_year=f.payroll_year
+where f.food_YoyGrowth -w.wage_YoyGrowth >5
+order by difference desc 
