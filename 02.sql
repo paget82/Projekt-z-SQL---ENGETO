c846002898180378666ae74ddf3e5157f8c19b0d
Patrik Pagac <patrik.pagac@gmail.com>
2025-09-26 21:11:20 +0200

    Add files via upload

--- 02.sql
+++ 02.sql
@@ -1,12 +1,12 @@
 --2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
-with YearIncome as(   
-		select 
-			payroll_year,
-			trunc(avg(average_wages)::numeric, 0)as average_salary
-		from t_patrik_pagac_project_SQL_primary_final
-		where calculation_code=200
-		group by payroll_year
+with Year_Income as(   
+	select 
+		payroll_year,
+		trunc(avg(average_wages)::numeric, 0)as average_salary
+	from t_patrik_pagac_project_SQL_primary_final
+	where calculation_code=200
+	group by payroll_year
 ),
 Milk_Bread as(
 	select  
@@ -18,19 +18,19 @@ Milk_Bread as(
 		and food_category in ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
 	group by payroll_year, food_category
 ),
-YearsRange as(
+Years_Range as(
 	select min(payroll_year)as min_year,
 			   max(payroll_year)as max_year
 	from Milk_Bread
 )
 select 
-		m.food_category,
-		m.payroll_year,
-		y.average_salary,
-		 m.average_price,
-		trunc((average_salary/ 	average_price) ,0) as count
+	m.food_category,
+	m.payroll_year,
+	y.average_salary,
+	 m.average_price,
+	trunc((average_salary/ 	average_price) ,0) as count
 from Milk_Bread m
-join YearIncome y
+join Year_Income y
 on y.payroll_year=m.payroll_year
-join YearsRange r
+join Years_Range r
 on m.payroll_year=r.min_year or m.payroll_year=r.max_year
