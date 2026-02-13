355fe9fbfc9ff78382b9b4af54351dc23e1d4283
Patrik Pagac <patrik.pagac@gmail.com>
2026-02-13 23:43:46 +0100

    Refactor SQL query

--- 05.sql
+++ 05.sql
@@ -1,47 +1,44 @@
 --5. Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
    		
-with YoyFoodGrowth  as(
-	select 
-			payroll_year,
-		    round(
-		          ((avg(price) - lag(avg(price)) over(order by payroll_year))
-		         / lag(avg(price)) over(order by payroll_year))::numeric* 100
-         , 2  )as food_YoyGrowth
-	from t_patrik_pagac_project_SQL_primary_final
-	group by  payroll_year
-	),
-YoyWageGrowth  as(
-    select
-    		payroll_year,
-    		round(
-	            ((avg(average_wages) - lag(avg(average_wages)) over (order by payroll_year))
-	             / lag(avg(average_wages)) over (order by payroll_year)) * 100
-         , 2  ) as wage_YoyGrowth
-    from t_patrik_pagac_project_SQL_primary_final
-    where calculation_code = 200
-    group by payroll_year
-    ),
-YoyGdp  as(  
-	select 
-			year, 
-			gdp,
-			round(
-				((gdp-lag(gdp)over(order by year))
-				/lag(gdp)over(order by year))::numeric* 100
-			,	2  )as gdp_YoyGrowth
-	from t_patrik_pagac_project_SQL_secondary_final
-	where country = 'Czech Republic'
+WITH yoy_food_growth AS (
+	SELECT
+		payroll_year,
+		ROUND(((AVG(price) - LAG(AVG(price)) OVER (ORDER BY payroll_year))
+		/ LAG(AVG(price)) OVER (ORDER BY payroll_year))::NUMERIC * 100, 2) AS yoy_food_growth
+	FROM t_patrik_pagac_project_SQL_primary_final
+	GROUP BY payroll_year
+),
+
+yoy_wage_growth AS (
+	SELECT
+		payroll_year,
+		ROUND(((AVG(average_wages) - LAG(AVG(average_wages)) OVER (ORDER BY payroll_year))
+		/ LAG(AVG(average_wages)) OVER (ORDER BY payroll_year)) * 100, 2) AS wage_yoy_growth
+	FROM t_patrik_pagac_project_SQL_primary_final
+	WHERE calculation_code = 200
+	GROUP BY payroll_year
+),
+
+yoy_gdp AS (  
+	SELECT 
+		year, 
+		gdp,
+		ROUND(((gdp - LAG(gdp) OVER (ORDER BY year))
+		/ LAG(gdp) OVER (ORDER BY year))::NUMERIC * 100, 2) AS gdp_yoy_growth
+	FROM t_patrik_pagac_project_SQL_secondary_final
+	WHERE country = 'Czech Republic'
 )
-select 
+
+SELECT
 	f.payroll_year, 
-	g.gdp_YoyGrowth,
-	w.wage_YoyGrowth,
-	f.food_YoyGrowth,
-	g.gdp_YoyGrowth,
-	lead(w.wage_YoyGrowth)over (order by f.payroll_year)as wage_next_year,
-	lead(f.food_YoyGrowth)over (order by f.payroll_year)as food_next_year
-from YoyFoodGrowth f
-join YoyWageGrowth w
-	on f.payroll_year=w.payroll_year
-join YoyGdp g
-	on f.payroll_year=g.year
+	g.gdp_yoy_growth,
+	w.wage_yoy_growth,
+	f.food_yoy_growth,
+	g.gdp_yoy_growth,
+	LEAD(w.wage_yoy_growth) OVER (ORDER BY f.payroll_year) AS wage_next_year,
+	LEAD(f.food_yoy_growth) OVER (ORDER BY f.payroll_year) AS food_next_year
+FROM yoy_food_growth AS f
+JOIN yoy_wage_growth AS w
+	ON f.payroll_year = w.payroll_year
+JOIN yoy_gdp AS g
+	ON f.payroll_year = g.year;
