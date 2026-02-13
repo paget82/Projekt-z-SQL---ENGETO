c1b9c1ee17d181609363eadf4b6fd40c0b32306f
Patrik Pagac <patrik.pagac@gmail.com>
2026-02-13 22:23:32 +0100

    Refactor SQL query

--- 01.sql
+++ 01.sql
@@ -1,25 +1,26 @@
 --1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
-with List as(
-	select 
-		industry,
-		payroll_year,
-		avg(average_wages)as average_salary
-	from t_patrik_pagac_project_SQL_primary_final
-	where calculation_code=200
-	group by industry, payroll_year
-	order by payroll_year
+WITH list AS (
+    SELECT 
+        industry,
+        payroll_year,
+        AVG(average_wages) AS average_salary
+    FROM t_patrik_pagac_project_SQL_primary_final
+    WHERE calculation_code = 200
+    GROUP BY industry, payroll_year
+
 )
-select 
+SELECT
     industry,
     payroll_year,
     average_salary,
-    lag(average_salary) over (partition by industry order by payroll_year) as average_salary_last_year,
-    average_salary - lag(average_salary) over (partition by industry order by payroll_year) as difference,
-    case 
-	    	when average_salary - lag(average_salary) over (partition by industry order by payroll_year)> 0 then 'increases'
-		    when average_salary - lag(average_salary) over (partition by industry order by payroll_year)= 0 then 'stagnates'
-		    when average_salary - lag(average_salary) over (partition by industry order by payroll_year)< 0 then 'decreases'
-		    else 'unknown'
-end as trend
-    from List
+    LAG(average_salary) OVER (PARTITION BY industry ORDER BY payroll_year) AS average_salary_last_year,
+    average_salary - LAG(average_salary) OVER (PARTITION BY industry ORDER BY payroll_year) AS difference,
+    CASE
+        WHEN average_salary - LAG(average_salary) OVER (PARTITION BY industry ORDER BY payroll_year) > 0 THEN 'increases'
+        WHEN average_salary - LAG(average_salary) OVER (PARTITION BY industry ORDER BY payroll_year) = 0 THEN 'stagnates'
+        WHEN average_salary - LAG(average_salary) OVER (PARTITION BY industry ORDER BY payroll_year) < 0 THEN 'decreases'
+        ELSE 'unknown'
+    END AS trend
+FROM list
+ORDER BY industry, payroll_year;
