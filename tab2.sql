52fa37d5409408262137a4787b2d3f2b6d017d49
Patrik Pagac <patrik.pagac@gmail.com>
2026-02-13 23:54:03 +0100

    Refactor SQL table creation

--- tab2.sql
+++ tab2.sql
@@ -1,15 +1,14 @@
-create table t_patrik_pagac_project_SQL_secondary_final as
-	select 
-		e.country,
-		c.continent,
-		e.year,
-		e.gdp,
-		e.population,
-		e.gini
-	from economies e
-		join countries c 
-		on c.country=e.country
-	where 1=1
-		and c.continent='Europe' 
-		and e.year between '2006' and '2018'
-	order by e.year asc
+CREATE TABLE t_patrik_pagac_project_SQL_secondary_final AS
+SELECT 
+	e.country,
+	c.continent,
+	e.year,
+	e.gdp,
+	e.population,
+	e.gini
+FROM economies AS e
+JOIN countries AS c 
+	ON c.country = e.country
+WHERE c.continent = 'Europe' 
+	AND e.year BETWEEN '2006' AND '2018'
+ORDER BY e.year;
