40022a689c637f5882ccc868cecacd71c61b55fd
Patrik Pagac <patrik.pagac@gmail.com>
2026-02-13 23:22:25 +0100

    Refactor SQL query

--- 04.sql
+++ 04.sql
@@ -1,33 +1,31 @@
 --4. Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
 
-with FoodGrowth  as(
-	select 
+WITH food_growth AS (
+	SELECT
 		payroll_year,
-	    round(
-		      ((avg(price) - lag(avg(price)) over (order by payroll_year))
-		     / lag(avg(price)) over (order by payroll_year))::numeric* 100
-     , 2  )as food_YoyGrowth
-	from t_patrik_pagac_project_SQL_primary_final
-	group by  payroll_year
-	),
-	WageGrowth  as(
-    select
-    	payroll_year,
-    	round(
-		        ((avg(average_wages) - lag(avg(average_wages)) over (order by payroll_year))
-		        / lag(avg(average_wages)) over (order by payroll_year)) * 100
-        , 2  ) as wage_YoyGrowth
-	from t_patrik_pagac_project_SQL_primary_final
-    where calculation_code = 200
-    group by payroll_year
-    )
-select
+	    ROUND(((AVG(price) - LAG(AVG(price)) OVER (ORDER BY payroll_year))
+		/ LAG(AVG(price)) OVER (ORDER BY payroll_year))::NUMERIC * 100, 2) AS food_yoy_growth
+	FROM t_patrik_pagac_project_SQL_primary_final
+	GROUP BY payroll_year
+),
+
+wage_growth AS (
+	SELECT
+		payroll_year,
+		ROUND(((AVG(average_wages) - LAG(AVG(average_wages)) OVER (ORDER BY payroll_year))
+		/ LAG(AVG(average_wages)) OVER (ORDER BY payroll_year)) * 100, 2) AS wage_YoyGrowth
+	FROM t_patrik_pagac_project_SQL_primary_final
+	WHERE calculation_code = 200
+	GROUP BY payroll_year
+)
+
+SELECT
 	w.payroll_year,
-	f.food_YoyGrowth,
-	w.wage_YoyGrowth,
-	 f.food_YoyGrowth -w.wage_YoyGrowth as  difference
-from FoodGrowth f
-join WageGrowth w
-	on w.payroll_year=f.payroll_year
-where f.food_YoyGrowth -w.wage_YoyGrowth >5
-order by difference desc 
+	f.food_yoy_growth,
+	w.wage_yoy_growth,
+	f.food_yoy_growth -w.wage_yoy_growth AS difference
+FROM food_growth AS f
+JOIN wage_growth AS w
+	ON w.payroll_year = f.payroll_year
+WHERE f.food_yoy_growth - w.wage_yoy_growth > 5
+ORDER BY difference DESC;
