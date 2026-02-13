8752b58da193c838f2b24e14c170445aab94e283
Patrik Pagac <patrik.pagac@gmail.com>
2026-02-13 23:49:48 +0100

    Refactor SQL table creation

--- tab1.sql
+++ tab1.sql
@@ -1,21 +1,20 @@
-create table t_patrik_pagac_project_SQL_primary_final as
-	select 
-		cpc.name as food_category,
-		cp.value as price,
-		cpib.name as industry,
-		cp2.value as average_wages,
-		to_char(cp.date_from, 'DD, Month YYYY') as price_mesured_from,
-		to_char(cp.date_to, 'DD, Month YYYY') as price_mesured_to,
-		cp2.payroll_year,
-		calculation_code
-	from 
-		czechia_price cp  
-	join czechia_payroll cp2   
-		on date_part('year', cp.date_from)=cp2.payroll_year
-		and cp2.value_type_code = 5958 
-		and  cp.region_code is null
-	join czechia_price_category cpc  
-		on cp.category_code=cpc.code 	
-	join czechia_payroll_industry_branch cpib 
-		on cp2.industry_branch_code=cpib.code  
-	order by payroll_year asc;
+CREATE TABLE t_patrik_pagac_project_SQL_primary_final AS
+SELECT
+	cpc.name AS food_category,
+	cp.value AS price,
+	cpib.name AS industry,
+	cp2.value AS average_wages,
+	TO_CHAR(cp.date_from, 'DD, Month YYYY') AS price_mesured_from,
+	TO_CHAR(cp.date_to, 'DD, Month YYYY') AS price_mesured_to,
+	cp2.payroll_year,
+	calculation_code
+FROM czechia_price AS cp  
+JOIN czechia_payroll AS cp2   
+	ON DATE_PART('year', cp.date_from) = cp2.payroll_year
+	AND cp2.value_type_code = 5958 
+	AND cp.region_code IS NULL
+JOIN czechia_price_category AS cpc  
+	ON cp.category_code = cpc.code 	
+JOIN czechia_payroll_industry_branch AS cpib 
+	ON cp2.industry_branch_code = cpib.code  
+ORDER BY payroll_year;
