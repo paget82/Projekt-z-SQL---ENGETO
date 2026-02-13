26bb5a8554426d5948fdefde71075848666c2b1d
Patrik Pagac <patrik.pagac@gmail.com>
2026-02-13 23:10:12 +0100

    Refactor SQL query

--- 03.sql
+++ 03.sql
@@ -1,29 +1,29 @@
 --3. Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? 
  
-with AvgPrices  as(
-	select 
-			food_category,
-			payroll_year,
-			round(avg(price	)::numeric, 2)as avg_price
-	from t_patrik_pagac_project_SQL_primary_final
-	group by food_category, payroll_year
+WITH avg_prices AS (
+	SELECT
+		food_category,
+		payroll_year,
+		ROUND(AVG(price)::NUMERIC, 2) AS avg_price
+	FROM t_patrik_pagac_project_SQL_primary_final
+	GROUP BY food_category, payroll_year
 ),
-YoYGrowth as(
-	select 
+
+yoy_growth AS (
+	SELECT
 		food_category,
 		payroll_year,
 		avg_price,
-		lag(avg_price) over (partition by food_category order by payroll_year) as prev_price,
-		round(
-		(	avg_price -  lag(avg_price) over (partition by food_category order by payroll_year))/ 
-		nullif(lag(avg_price) over (partition by food_category order by payroll_year  ),0)*100
-		, 2)  as yoy_growth_percent
-	from AvgPrices 
+		LAG(avg_price) OVER (PARTITION BY food_category ORDER BY payroll_year) AS prev_price,
+		ROUND((avg_price -  LAG(avg_price) OVER (PARTITION BY food_category ORDER BY payroll_year)) / 
+		NULLIF(LAG(avg_price) OVER (PARTITION BY food_category ORDER BY payroll_year), 0) * 100, 2) AS yoy_growth_percent
+	FROM avg_prices
+
 )
-select 
+SELECT 
 	food_category,
-	round(avg(yoy_growth_percent),2)as avg_growth
-from YoYGrowth 
-where yoy_growth_percent is not null
-group  by food_category
-order by avg_growth asc
+	ROUND(AVG(yoy_growth_percent), 2) AS avg_growth
+FROM yoy_growth
+WHERE yoy_growth_percent IS NOT NULL
+GROUP BY food_category
+ORDER BY avg_growth;
